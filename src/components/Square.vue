30985f233c90007b7535e9d5c103b99778d4fe3c
Plk <plikforo@gmail.com>
2025-04-01 17:25:56 +0200

    feat: :sparkles: Enhance Square component with multi-colored cube and OrbitControls for improved interaction

--- src/components/Square.vue
+++ src/components/Square.vue
@@ -2,13 +2,14 @@
 import { onMounted, onBeforeUnmount, ref } from "vue";
 import * as THREE from "three";
 import gsap from "gsap";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 
 const canvas = ref<HTMLCanvasElement>();
 let scene: THREE.Scene;
 let camera: THREE.PerspectiveCamera;
 let renderer: THREE.WebGLRenderer;
 let cube: THREE.Mesh;
-
+let controls: OrbitControls;
 const init = () => {
   scene = new THREE.Scene();
   scene.background = new THREE.Color("#000000");
@@ -21,9 +22,19 @@ const init = () => {
   scene.add(camera);
 
   const geometry = new THREE.BoxGeometry(1, 1, 1);
-  const material = new THREE.MeshBasicMaterial({ color: "#00ff00" });
-  cube = new THREE.Mesh(geometry, material);
+  const materials = [
+    new THREE.MeshBasicMaterial({ color: "#0000ff" }), // right (azul)
+    new THREE.MeshBasicMaterial({ color: "#ff0000" }), // left (rojo)
+    new THREE.MeshBasicMaterial({ color: "#00ff00" }), // top (verde)
+    new THREE.MeshBasicMaterial({ color: "#ff69b4" }), // bottom (rosa)
+    new THREE.MeshBasicMaterial({ color: "#800080" }), // front (morado)
+    new THREE.MeshBasicMaterial({ color: "#ffff00" }), // back (amarillo)
+  ];
+  cube = new THREE.Mesh(geometry, materials);
   scene.add(cube);
+
+  controls = new OrbitControls(camera, canvas.value!);
+  controls.enableDamping = true;
 
   renderer = new THREE.WebGLRenderer({ canvas: canvas.value });
   renderer.setSize(canvas.value!.clientWidth, canvas.value!.clientHeight);
@@ -31,14 +42,16 @@ const init = () => {
 };
 
 const animate = () => {
-  gsap.to(cube.rotation, {
-    y: cube.rotation.y + Math.PI * 0.5,
-    duration: 2,
-    ease: "none",
-    repeat: -1,
-  });
+  // gsap.to(cube.rotation, {
+  //   y: cube.rotation.y + Math.PI * 2,
+  //   duration: 2,
+  //   ease: "none",
+  //   repeat: -1,
+  // });
 
+  
   const render = () => {
+    controls.update();
     renderer.render(scene, camera);
     requestAnimationFrame(render);
   };
