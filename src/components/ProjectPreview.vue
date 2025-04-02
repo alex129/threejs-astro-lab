b001df01c4640e31ed6567115c4a3da96525301f
Plk <plikforo@gmail.com>
2025-04-02 20:41:09 +0200

    feat: :sparkles: Add ProjectPreview component and update index.astro to display project cards with descriptions

---
+++ src/components/ProjectPreview.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="w-full h-[200px] overflow-hidden rounded-lg bg-gray-100">
+    <iframe
+      :src="url"
+      :title="title"
+      class="w-[200%] h-[200%] border-0 origin-top-left scale-50 pointer-events-none"
+      loading="lazy"
+    ></iframe>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  url: string;
+  title: string;
+}>();
+</script>
+
+<style>
+/* No needed styles as we're using Tailwind */
+</style>
